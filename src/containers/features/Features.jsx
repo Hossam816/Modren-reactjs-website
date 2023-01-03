e46bee5e5a99cee14e9704495da8ea02605cd826
Hossam Ragab <hossam.alasklany@gmail.com>
2023-01-03 14:55:58 +0200

    scratchDone

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './feature.css';
+
+function Features() {
+    return (
+        <div>Features</div>
+    );
+}
+
+export default Features;
---
+++ src/containers/whatGPT/GPT.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './gpt.css';
+
+function GPT() {
+    return (
+        <div>GPT</div>
+    );
+}
+
+export default GPT;
