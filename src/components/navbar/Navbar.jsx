019deb391c5940c189b5b1d765ffe1fb13a7d3cc
Hossam Ragab <hossam.alasklany@gmail.com>
2023-01-03 15:36:52 +0200

    adding assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import Logo from '../../assets/logo.svg';
+import './navbar.css';
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className='gpt__navbar'>
+      <div className='gpt__navbar-links'>
+        <div className='gpt__navbar-links_logo'>
+          <img src={ Logo } alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
